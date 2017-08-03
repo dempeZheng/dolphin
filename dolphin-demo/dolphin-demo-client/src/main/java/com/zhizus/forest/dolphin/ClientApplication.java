2b899879808bbac4dfbe854d1b2b2d4ea1f9df0a
dempezheng <dempezheng@kugou.net>
2017-08-03 12:54:07 +0800

    hystrix command fallback

--- dolphin-demo/dolphin-demo-client/src/main/java/com/zhizus/forest/dolphin/ClientApplication.java
+++ dolphin-demo/dolphin-demo-client/src/main/java/com/zhizus/forest/dolphin/ClientApplication.java
@@ -1,6 +1,5 @@
 package com.zhizus.forest.dolphin;
 
-import com.netflix.hystrix.contrib.javanica.aop.aspectj.HystrixCommandAspect;
 import com.zhizus.forest.dolphin.annotation.Inject;
 import com.zhizus.forest.dolphin.gen.Sample;
 import com.zhizus.forest.dolphin.hystrix.SampleClientCommand;
@@ -11,7 +10,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.context.annotation.Bean;
 
 /**
  * Created by Dempe on 2017/7/1 0001.
@@ -23,7 +21,6 @@ public class ClientApplication implements CommandLineRunner {
 
     @Inject("sampleClient")
     Sample.Client client;
-
     @Autowired
     SampleClientCommand sampleClientCommand;
 
@@ -32,19 +29,19 @@ public class ClientApplication implements CommandLineRunner {
         app.run(args);
 
     }
-//
-//    @Bean
-//    public HystrixCommandAspect hystrixCommandAspect() {
-//        HystrixCommandAspect hystrixCommandAspect = new HystrixCommandAspect();
-//        System.out.println(">>>>>>>>>>>>"+hystrixCommandAspect);
-//        return hystrixCommandAspect;
-//    }
 
     @Override
     public void run(String... args) throws Exception {
+        runHello();
+        runHystrixCommandHello();
+    }
+
+    private void runHello() throws TException {
         String test = client.hello("test");
         System.out.println(test);
 
+    }
+    private void runHystrixCommandHello() throws TException {
         String command = sampleClientCommand.hello("command");
         System.out.println(command);
     }
