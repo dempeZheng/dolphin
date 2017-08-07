75c712877b11a1a957bcbcbe4f3a9cd025d3b6f7
dempeZheng <752996510@qq.com>
2017-08-07 21:34:16 +0800

    add eureka demo

---
+++ dolphin-demo/dolphin-demo-eureka/src/main/java/com/zhizus/forest/dolphin/demo/eureka/Application.java
@@ -0,0 +1,15 @@
+package com.zhizus.forest.dolphin.demo.eureka;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(Application.class).web(true).run(args);
+	}
+
+}
